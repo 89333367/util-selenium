963b3b7c5296f594eaeec93a20377514fae62b06
SunYu <sunyu@uml-tech.com>
2024-11-23 17:26:51 +0800

    init

--- src/test/resources/addCheckbox.js
+++ src/test/resources/addCheckbox.js
@@ -13,11 +13,11 @@ if (__exportCheckbox) {
     // 设置复选框的样式
     checkbox.style.width = '50px';       // 设置宽度
     checkbox.style.height = '50px';      // 设置高度
-    checkbox.style.border = '1px solid #000'; // 设置边框
-    checkbox.style.backgroundColor = '#f0f0f0'; // 设置背景色
-    checkbox.style.boxShadow = '0 0 2px rgba(0,0,0,0.2)'; // 设置阴影
     checkbox.style.cursor = 'pointer';   // 更改鼠标悬停时的光标样式
     checkbox.style.margin = '10px'; // 可以设置外边距，根据需要调整
+    //checkbox.style.border = '1px solid #000'; // 设置边框
+    //checkbox.style.backgroundColor = '#f0f0f0'; // 设置背景色
+    //checkbox.style.boxShadow = '0 0 2px rgba(0,0,0,0.2)'; // 设置阴影
 
     // 步骤3: 创建标签
     let label = document.createElement('label');
@@ -29,8 +29,8 @@ if (__exportCheckbox) {
     label.style.fontSize = '20px'; // 放大字体
     label.style.backgroundColor = 'red'; // 背景色为红色
     label.style.color = 'white'; // 设置字体颜色为白色，以便在红色背景上可见
-    label.style.padding = '10px'; // 添加一些内边距
     label.style.display = 'inline-block'; // 使 label 表现为块级元素，以便样式生效
+    label.style.padding = '10px'; // 添加一些内边距
 
     // 步骤4: 将复选框和标签添加到 div 中
     div.appendChild(checkbox);
