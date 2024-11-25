815fee029f9b9cd42d19a530b3f042c254cdc2f3
SunYu <sunyu@uml-tech.com>
2024-11-25 16:07:38 +0800

    init

--- src/test/java/sunyu/util/TestUtil.java
+++ src/test/java/sunyu/util/TestUtil.java
@@ -84,31 +84,27 @@ public class TestUtil {
         seleniumUtil.executeJavascript(ResourceUtil.readUtf8Str("showMessage.js"), "准备导出数据....");
 
         log.info("页面类型 {}", pageType.get());
+        String preText = null;//记录上一页第一行的内容，用来对比翻页是否生效
         if (pageType.get() == 1) {
             //共7474条， 第1/499页
             //共7条， 第1/1页
             String s = seleniumUtil.waitVisibilityOfElementLocatedByCssSelector("div.divPage>span").getText();
             int totalPage = Convert.toInt(ReUtil.getGroup1("/(\\d+)页", s));
             log.info("准备导出 {} 页数据", totalPage);
+            //获取表头
+            for (WebElement th : seleniumUtil.waitVisibilityOfAllElementsLocatedByCssSelector("table[role='c-table']>thead>tr>th")) {
+                log.info("{}", th.getText());
+            }
             for (int i = 1; i <= totalPage; i++) {
-                if (i == 1) {
-                    //获取表头
-                    for (WebElement th : seleniumUtil.waitVisibilityOfAllElementsLocatedByCssSelector("table[role='c-table']>thead>tr>th")) {
-                        log.info("{}", th.getText());
-                    }
-                } else {//大于1页的时候才需要点击页码进行翻页
+                if (i > 1) {//大于1页的时候才需要点击页码进行翻页
                     log.info("翻页到 {}", i);
                     for (WebElement el : seleniumUtil.waitVisibilityOfAllElementsLocatedByCssSelector("div.pagerItem>a")) {
                         if (el.getText().equals(Convert.toStr(i))) {
                             el.click();//点击页码，进行翻页
-                            log.info("等待翻页成功");
-                            seleniumUtil.waitVisibilityOfElementLocatedByCssSelector("div.pagerItem>a.current");//等待选中
-                            log.info("翻页成功");
                             break;
                         }
                     }
                 }
-                seleniumUtil.executeJavascript(ResourceUtil.readUtf8Str("showMessage.js"), StrUtil.format("导出进度 {}/{}", i, totalPage));
                 List<WebElement> trs = null;
                 while (trs == null) {
                     try {
@@ -117,7 +113,14 @@ public class TestUtil {
                         log.warn("获取数据异常，重试");
                     }
                 }
+                if (preText != null && preText.equals(trs.get(0).getText())) {
+                    log.warn("翻页不成功，重新翻页");
+                    i--;
+                    continue;
+                }
+                preText = trs.get(0).getText();
                 log.info("页码 {} 有 {} 行数据", i, trs.size());
+                seleniumUtil.executeJavascript(ResourceUtil.readUtf8Str("showMessage.js"), StrUtil.format("导出进度 {}/{}", i, totalPage));
             }
         } else if (pageType.get() == 2) {
             // 共229341条，第1/15290页
@@ -184,5 +187,18 @@ public class TestUtil {
         s = " 共2条，第1/1页 ";
         log.info("{}", ReUtil.getGroup1("第(\\d+)/", s));
         log.info("{}", ReUtil.getGroup1("/(\\d+)页", s));
+    }
+
+    @Test
+    void t004() {
+        for (int i = 1; i <= 10; i++) {
+            log.info("{}", i);
+            if (i == 5) {
+                i--;
+                ThreadUtil.sleep(1000);
+                continue;
+            }
+            ThreadUtil.sleep(1000);
+        }
     }
 }
