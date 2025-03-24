8abdd0472ff12bbe70d49b26d33eda3994733987
SunYu <sunyu@uml-tech.com>
2025-03-24 13:48:17 +0800

    [重构] 代码重构

--- src/main/java/sunyu/util/SeleniumUtil.java
+++ src/main/java/sunyu/util/SeleniumUtil.java
@@ -20,8 +20,6 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.Closeable;
-import java.io.Serializable;
 import java.time.Duration;
 import java.util.List;
 import java.util.function.Function;
@@ -31,22 +29,102 @@ import java.util.function.Function;
  *
  * @author 孙宇
  */
-public class SeleniumUtil implements Serializable, Closeable {
+public class SeleniumUtil implements AutoCloseable {
     private final Log log = LogFactory.get();
+    private final Config config;
 
-    private WebDriverManager webDriverManager;
-    private WebDriver webDriver;
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private SeleniumUtil(Config config) {
+        log.info("[构建SeleniumUtil] 开始");
+        if (config.webDriverManager == null) {
+            try {
+                log.info("[选择驱动] 尝试选择 chrome 驱动");
+                config.webDriverManager = WebDriverManager.chromedriver();
+                config.webDriverManager.disableCsp();
+                log.info("[配置驱动] 配置 chrome web driver");
+                config.webDriverManager.setup();
+                ChromeOptions options = new ChromeOptions();
+                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation", "disable-popup-blocking"});// 禁用浏览器的自动测试软件提示，禁用 Chrome 的弹出窗口拦截功能
+                options.addArguments("--remote-allow-origins=*", "disable-search-engine-choice-screen");//解决 403 出错问题，禁用Chrome浏览器在启动时显示的搜索引擎选择界面
+                //options.setHeadless(true);//这句话作用是不用打开浏览器
+                config.webDriver = new ChromeDriver(options);
+            } catch (Exception e) {
+                config.webDriverManager = null;
+                log.warn(e);
+            }
+        }
+        if (config.webDriverManager == null) {
+            try {
+                log.info("[选择驱动] 尝试选择 edge 驱动");
+                config.webDriverManager = WebDriverManager.edgedriver();
+                config.webDriverManager.disableCsp();
+                log.info("[配置驱动] 配置 edge web driver");
+                config.webDriverManager.setup();
+                EdgeOptions options = new EdgeOptions();
+                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});// 禁用浏览器的自动测试软件提示
+                options.addArguments("--remote-allow-origins=*", "--disable-features=Sync");//解决 403 出错问题，屏蔽Microsoft Edge浏览器在启动时弹出的同步用户配置和个性化设置的提示
+                config.webDriver = new EdgeDriver(options);
+            } catch (Exception e) {
+                config.webDriverManager = null;
+                log.warn(e);
+            }
+        }
+        if (config.webDriverManager == null) {
+            try {
+                log.info("[选择驱动] 尝试选择 firefox 驱动");
+                config.webDriverManager = WebDriverManager.firefoxdriver();
+                config.webDriverManager.disableCsp();
+                log.info("[配置驱动] 配置 firefox web driver");
+                config.webDriverManager.setup();
+                FirefoxOptions options = new FirefoxOptions();
+                config.webDriver = new FirefoxDriver(options);
+            } catch (Exception e) {
+                config.webDriverManager = null;
+                log.warn(e);
+            }
+        }
+        log.info("[构建SeleniumUtil] 结束");
+
+        this.config = config;
+    }
+
+    private static class Config {
+        private WebDriverManager webDriverManager;
+        private WebDriver webDriver;
+    }
+
+    public static class Builder {
+        private final Config config = new Config();
+
+        public SeleniumUtil build() {
+            return new SeleniumUtil(config);
+        }
+    }
+
+    /**
+     * 回收资源
+     */
+    @Override
+    public void close() {
+        log.info("[销毁SeleniumUtil] 开始");
+        config.webDriver.quit();
+        killDriverProcess();
+        log.info("[销毁SeleniumUtil] 结束");
+    }
 
     /**
      * 资源回收
      */
     private void killDriverProcess() {
         if (SystemUtil.getOsInfo().isWindows()) {
-            log.info("删除残留驱动开始");
+            log.info("[删除残留驱动] 开始");
             RuntimeUtil.exec("taskkill /f /im chromedriver.exe");
             RuntimeUtil.exec("taskkill /f /im msedgedriver.exe");
             RuntimeUtil.exec("taskkill /f /im geckodriver.exe");
-            log.info("删除残留驱动结束");
+            log.info("[删除残留驱动] 结束");
         }
     }
 
@@ -56,7 +134,7 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @return
      */
     public WebDriver getWebDriver() {
-        return webDriver;
+        return config.webDriver;
     }
 
     /**
@@ -65,21 +143,21 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @param url
      */
     public void openUrl(String url) {
-        webDriver.get(url);
+        config.webDriver.get(url);
     }
 
     /**
      * 最大化窗口
      */
     public void maxWindow() {
-        webDriver.manage().window().maximize();
+        config.webDriver.manage().window().maximize();
     }
 
     /**
      * 最小化窗口
      */
     public void minWindow() {
-        webDriver.manage().window().minimize();
+        config.webDriver.manage().window().minimize();
     }
 
     /**
@@ -88,7 +166,7 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @return
      */
     public String getCurrentUrl() {
-        return webDriver.getCurrentUrl();
+        return config.webDriver.getCurrentUrl();
     }
 
     /**
@@ -97,7 +175,7 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @return
      */
     public String getTitle() {
-        return webDriver.getTitle();
+        return config.webDriver.getTitle();
     }
 
     /**
@@ -117,14 +195,14 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @param args
      */
     public void executeJavascript(String script, Object... args) {
-        ((JavascriptExecutor) webDriver).executeScript(script, args);
+        ((JavascriptExecutor) config.webDriver).executeScript(script, args);
     }
 
     /**
      * 只保留最新窗口
      */
     public void keepLastWindow() {
-        closeOtherWindow(CollUtil.getLast(webDriver.getWindowHandles()));
+        closeOtherWindow(CollUtil.getLast(config.webDriver.getWindowHandles()));
     }
 
     /**
@@ -133,12 +211,12 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @param keepNameOrHandle
      */
     public void closeOtherWindow(String keepNameOrHandle) {
-        for (String windowHandle : webDriver.getWindowHandles()) {
+        for (String windowHandle : config.webDriver.getWindowHandles()) {
             if (!windowHandle.equals(keepNameOrHandle)) {
-                webDriver.switchTo().window(windowHandle).close();
+                config.webDriver.switchTo().window(windowHandle).close();
             }
         }
-        webDriver.switchTo().window(keepNameOrHandle);
+        config.webDriver.switchTo().window(keepNameOrHandle);
     }
 
     /**
@@ -201,7 +279,7 @@ public class SeleniumUtil implements Serializable, Closeable {
     public WebElement findElementByCssSelector(String cssSelector) {
         //定位元素
         By by = By.cssSelector(cssSelector);
-        return webDriver.findElement(by);
+        return config.webDriver.findElement(by);
     }
 
     /**
@@ -213,7 +291,7 @@ public class SeleniumUtil implements Serializable, Closeable {
     public List<WebElement> findElementsByCssSelector(String cssSelector) {
         //定位元素
         By by = By.cssSelector(cssSelector);
-        return webDriver.findElements(by);
+        return config.webDriver.findElements(by);
     }
 
     /**
@@ -366,100 +444,9 @@ public class SeleniumUtil implements Serializable, Closeable {
      * @return
      */
     public <V> V webDriverWaitUntil(Duration waitTime, Function<? super WebDriver, V> condition) {
-        WebDriverWait webDriverWait = new WebDriverWait(webDriver, waitTime);
+        WebDriverWait webDriverWait = new WebDriverWait(config.webDriver, waitTime);
         //等待条件成立后返回元素引用
         return webDriverWait.until(condition);
-    }
-
-    private void buildWebDriver() {
-        if (webDriverManager == null) {
-            try {
-                log.info("尝试选择 chrome 驱动");
-                webDriverManager = WebDriverManager.chromedriver();
-                webDriverManager.disableCsp();
-                log.info("配置 chrome web driver");
-                webDriverManager.setup();
-                ChromeOptions options = new ChromeOptions();
-                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation", "disable-popup-blocking"});// 禁用浏览器的自动测试软件提示，禁用 Chrome 的弹出窗口拦截功能
-                options.addArguments("--remote-allow-origins=*", "disable-search-engine-choice-screen");//解决 403 出错问题，禁用Chrome浏览器在启动时显示的搜索引擎选择界面
-                //options.setHeadless(true);//这句话作用是不用打开浏览器
-                webDriver = new ChromeDriver(options);
-            } catch (Exception e) {
-                webDriverManager = null;
-                log.warn(e);
-            }
-        }
-        if (webDriverManager == null) {
-            try {
-                log.info("尝试选择 edge 驱动");
-                webDriverManager = WebDriverManager.edgedriver();
-                webDriverManager.disableCsp();
-                log.info("配置 edge web driver");
-                webDriverManager.setup();
-                EdgeOptions options = new EdgeOptions();
-                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});// 禁用浏览器的自动测试软件提示
-                options.addArguments("--remote-allow-origins=*", "--disable-features=Sync");//解决 403 出错问题，屏蔽Microsoft Edge浏览器在启动时弹出的同步用户配置和个性化设置的提示
-                webDriver = new EdgeDriver(options);
-            } catch (Exception e) {
-                webDriverManager = null;
-                log.warn(e);
-            }
-        }
-        if (webDriverManager == null) {
-            try {
-                log.info("尝试选择 firefox 驱动");
-                webDriverManager = WebDriverManager.firefoxdriver();
-                webDriverManager.disableCsp();
-                log.info("配置 firefox web driver");
-                webDriverManager.setup();
-                FirefoxOptions options = new FirefoxOptions();
-                webDriver = new FirefoxDriver(options);
-            } catch (Exception e) {
-                webDriverManager = null;
-                log.warn(e);
-            }
-        }
-    }
-
-
-    /**
-     * 私有构造，避免外部初始化
-     */
-    private SeleniumUtil() {
-    }
-
-    /**
-     * 获得工具类工厂
-     *
-     * @return
-     */
-    public static SeleniumUtil builder() {
-        return new SeleniumUtil();
-    }
-
-    /**
-     * 构建工具类
-     *
-     * @return
-     */
-    public SeleniumUtil build() {
-        log.info("构建工具类开始");
-        killDriverProcess();
-        buildWebDriver();
-        log.info("构建工具类结束");
-        return this;
-    }
-
-
-    /**
-     * 销毁工具
-     */
-    @Override
-    public void close() {
-        log.info("销毁工具类开始");
-        webDriver.quit();
-        killDriverProcess();
-        log.info("销毁工具类结束");
     }
 
 }
